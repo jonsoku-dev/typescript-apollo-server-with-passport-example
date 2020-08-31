2082bb2e59a9ac9d2b4a4c86a1cecfe77417ea20
ricky <whdtjr2792@gmail.com>
2020-08-31 09:43:53 +0900

    typegrqphl setup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
+import { ApolloServer } from 'apollo-server-express';
+import Express from 'express';
+import { buildSchema, Query, Resolver } from 'type-graphql';
 import 'reflect-metadata';
-import { GraphQLServer } from 'graphql-yoga';
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async hello() {
+    return 'Hello World !';
   }
-`;
+}
 
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
-  },
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  });
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = Express();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('server started on http://localhost:4000');
+  });
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
-server.start(() => console.log('Server is running on localhost:4000'));
-
+main().catch((err) => console.error(err));
